0f6dd6cefda8fda8de40e6d8d4ffc6056b31880c
Admin <admin@Admins-MacBook-Pro.local>
2020-03-07 12:34:05 -0500

    added logic to dice-roll componenet

--- dice-wizard/ClientApp/src/app/dice-roller/dice-roller.component.ts
+++ dice-wizard/ClientApp/src/app/dice-roller/dice-roller.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../home/home.component.service';
+import { DiceRoll } from '../models/dice-roll.model';
 
 @Component({
   selector: 'app-dice-roller',
@@ -8,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class DiceRollerComponent implements OnInit {
   numberOfSides = 20;
   name: string | null;
-  constructor() { }
+  constructor(private homeService: HomeService) { }
 
   ngOnInit() {
   }
 
+  rollDice(): void{
+    let roll = new DiceRoll();
+    roll.name = name;
+    roll.diceRoll = Math.floor(Math.random() * this.numberOfSides) + 1;
+    this.homeService.diceRolls.push(roll);
+  }
 }
