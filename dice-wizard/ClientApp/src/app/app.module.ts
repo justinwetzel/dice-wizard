a894aee6e6587bcd6986207df75601af972594c7
Justin Wetzel <justinwetzel@mail.usf.edu>
2020-03-07 09:17:22 -0500

    Add base components for dice roller and leaderboards

--- dice-wizard/ClientApp/src/app/app.module.ts
+++ dice-wizard/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { DiceRollerComponent } from './dice-roller/dice-roller.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    DiceRollerComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
